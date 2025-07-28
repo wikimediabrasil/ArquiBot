b3c0b2738d2cb0ac23dd4cc06bfec1b768891e2f
Amarajah <naomi.ibeh69@gmail.com>
2025-07-28 10:00:06 -0700

    First batch of tests

--- arquibot/archivebot/tests.py
+++ arquibot/archivebot/tests.py
@@ -463,7 +463,6 @@ class TestUtils(unittest.TestCase):
         mock_extract_diff,
         mock_stats_update,
         mock_process_citation,
-        mock_log_info,
         mock_recent_changes,
         mock_archive_url,
         mock_is_alive,
