a08eb179fa54a76ec93ec6b37faa4e46c1adcb4c
arcstur <arcstur@gmail.com>
2025-11-10 13:21:54 -0300

    fix: STATIC_ROOT

--- src/arquibot/settings.py
+++ src/arquibot/settings.py
@@ -175,6 +175,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(BASE_DIR, "static"))
 LOGIN_REDIRECT_URL = '/stats/'
 
 # Default primary key field type
