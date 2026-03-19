c1c59058a0a9b254baf1ddd8cdfb43b81d0e4717
arcstur <arcstur@gmail.com>
2026-03-19 13:55:03 -0300

    refactor: runner runs for yesterday's RC and waits until tomorrow

--- src/archivebot/management/commands/run_archive_bot.py
+++ src/archivebot/management/commands/run_archive_bot.py
@@ -1,39 +1,63 @@
-from django.core.management.base import BaseCommand, CommandError
-from archivebot.utils import run_archive_bot
 import time
 import logging
+from datetime import timedelta
+from datetime import UTC
+
+from django.core.management.base import BaseCommand
+from django.utils.timezone import now
+
+from archivebot.utils import run_rc_date
+from archivebot.models import ArticleCheck, Wikipedia
+
+logger = logging.getLogger("arquibot")
 
 
 class Command(BaseCommand):
-    help = "Run the archive bot every N hours (default: 24 hours)"
+    help = "Run daily the archive bot on yesterday's (UTC) Recent Changes"
+    HOURS = 24
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "--interval",
+            "--stop-at-edit-count",
             type=int,
-            default=168,  # default now is 168(1 week) hours
-            help="Interval in hours between bot runs (default: 24)"
+            default=10,
+            help="Stop after this many edits (default: 10)",
         )
 
     def handle(self, *args, **options):
-        interval = options["interval"]
-        if interval <= 0:
-            raise CommandError("Interval must be greater than 0")
-
-        logging.info(f"Starting archive bot. Running every {interval} hour(s).")
-
+        stop_at_edit_count = self.stop_at_edit_count(options["stop_at_edit_count"])
         while True:
-            try:
-                self.stdout.write(self.style.SUCCESS("Running archive bot cycle..."))
+            self.now = now()
+            yesterday = self.yesterday()
+            if not self.yesterday_already_checked():
+                try:
+                    run_rc_date(yesterday, stop_at_edit_count=stop_at_edit_count)
+                except Exception as e:
+                    logger.error(f"error for {yesterday} Recent Changes: {e}")
+            self.wait_until_tomorrow(yesterday)
 
-                # === Run your archive bot here ===
-                run_archive_bot(interval)
+    def yesterday(self):
+        today_utc = self.now.astimezone(UTC).date()
+        return today_utc - timedelta(days=1)
 
-                logging.info("Bot cycle completed successfully.")
+    def yesterday_already_checked(self):
+        wikipedia = Wikipedia.get()
+        return ArticleCheck.objects.filter(
+            wikipedia=wikipedia, created__date=self.yesterday()
+        ).exists()
 
-            except Exception as e:
-                logging.error(f"Error while running bot: {e}")
-                self.stderr.write(self.style.ERROR(f"Error: {e}"))
+    def wait_until_tomorrow(self):
+        tomorrow_6am = (self.now + timedelta(days=1)).replace(hours=6)
+        wait_sec = (tomorrow_6am - self.now).total_seconds()
+        wait_hours = wait_sec / 3600
+        logger.info(f"sleeping for {wait_hours:.3f}... hours")
+        time.sleep(wait_sec)
 
-            logging.info(f"Sleeping for {interval} hour(s)...")
-            time.sleep(interval * 3600)  # Convert hours to seconds
+    def stop_at_edit_count(self, stop_at_edit_coun_str: str = None):
+        try:
+            value = int(stop_at_edit_coun_str)
+            if value >= 1:
+                return value
+        except (ValueError, TypeError):
+            pass
+        return None
